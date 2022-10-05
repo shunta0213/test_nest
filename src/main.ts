d05d7d96ddaa55daac5d11110173d9719b5853a6
Shunta Ide <shuntaide16213@gmail.com>
2022-10-05 18:33:11 +0900

    Added Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // For Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Sample Rest API')
+    .setDescription('REST API tutorial in Nestjs')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
