361e94135df9fc698b774c8c93326fa49a68b808
Shunta Ide <shuntaide16213@gmail.com>
2022-10-05 17:45:14 +0900

    Added comment

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// Controller のUnit Test
+
 describe('AppController', () => {
   let appController: AppController;
 
